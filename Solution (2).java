9d3aec9cb3f1ce966671bb52770a3986b5a2a6c2
Dmitry <122464516+Chakci@users.noreply.github.com>
2023-01-17 03:56:49 +0300

    Add files via upload

---
+++ Solution (2).java
@@ -0,0 +1,14 @@
+/*В переменной number записано число.
+В переменную lastDigit нужно записать последнюю цифру этого числа.
+Для вычисления используй переменную number и оператор «остаток от деления».
+Для объявления и инициализации lastDigit используй одну команду.*/ 
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 546;
+
+        int lastDigit = number % 10; 
+
+        System.out.println(lastDigit);
+    }
+}
