08c67f85aa4deb8b8883e8edeae3c63edffb66e7
Dmitry <122464516+Chakci@users.noreply.github.com>
2023-01-29 15:40:18 +0300

    Add files via upload

---
+++ Поиск длины окружности с данным радиусом.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Solution {
+  
+    public static void main(String[] args) 
+    {
+      Scanner math = new Scanner(System.in);
+   double rad = math.nextInt();
+   double C = 2 * Math.PI * rad;
+   System.out.println("Длинна окружности = " + C);
+    }
+
+}
