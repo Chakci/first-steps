73362a122b446dd40ac77a9a7e1105992e7fbf65
Dmitry <122464516+Chakci@users.noreply.github.com>
2023-01-29 14:35:34 +0300

    Add files via upload

---
+++ Снекоторого момента прошло 234 дня.Сколько полных недель и месяцев прошло за этот период.java
@@ -0,0 +1,11 @@
+public class Solution {
+  
+    public static void main(String[] args) 
+    {
+      int a = 234;
+      int weeks = 234 / 7;
+      int month = 234 / 30;
+      System.out.printf("Прошло недель %d\nПрошло месяцев %d\n", weeks, month);
+    }
+
+}
